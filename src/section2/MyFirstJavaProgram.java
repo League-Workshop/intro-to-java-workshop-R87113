44b258b1ce4668cd863c9d67fe3725ad0269c5b6
ilabstaff <ilabstaff@172.16.116.73>
2018-12-05 17:53:14 -0800

    Spiral

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,6 +1,8 @@
 package section2;
 
 import java.awt.Color;
+import java.util.Iterator;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -8,8 +10,14 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
+		Robot chappie=new Robot();
+		chappie.penDown();
+		chappie.setSpeed(300);
+		for(int i=0;i<200;i++) {
+		chappie.move(100);
+		chappie.turn(90);
+		chappie.move(100);
 		
 	}
+}
 }
